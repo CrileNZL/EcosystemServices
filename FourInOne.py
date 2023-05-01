ced6fb83eec64d0da4eb677743355680f30163f5
CrileNZL <crile.doscher@lincoln.ac.nz>
2023-05-01 12:02:10 +1200

    1 May - removing logistic eqn normalising and resetting base normalising values.

--- FourInOne.py
+++ FourInOne.py
@@ -1,7 +1,7 @@
 # Calculate ecosystem services layers and metascores for individual ES rasters
 # Combines Cooling.py, Nitrogen.py, Laca.py and metascores.py into one script
 # Developed for Richard Morris
-# C. Doscher October 2022 - Updated 28 April 2023
+# C. Doscher October 2022 - Updated 1 May 2023
 
 import arcpy
 
