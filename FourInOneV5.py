7d9350bec4c392daae8f15e1b1f196334ba68578
CrileNZL <crile.doscher@lincoln.ac.nz>
2024-09-24 15:09:58 +1200

    24 Sept - pasted Test.py script for cooling and BB habitat with variable sizes for irregular polygons.

--- FourInOneV5.py
+++ FourInOneV5.py
@@ -31,22 +31,25 @@ cline = arcpy.GetParameterAsText(1)
 ws = arcpy.GetParameterAsText(2)
 arcpy.env.workspace = ws
 
+# get minimum distance divisor from user - used on line 236
 dcool = float(arcpy.GetParameterAsText(3))
 
+# get CC value from user - used on line 255
+cc = float(arcpy.GetParameterAsText(4))
 
 # Get boundary zone FC from user
-boundary = arcpy.GetParameterAsText(4)
+boundary = arcpy.GetParameterAsText(5)
 arcpy.env.extent = boundary
 
 # Get cell size from user
 # get cellsize from user
-cellSize = arcpy.GetParameterAsText(5)
+cellSize = arcpy.GetParameterAsText(6)
 # cellSizeHa = (float(cellSize)**2)/10000.0
 
 # get output file name from user
-outName = arcpy.GetParameterAsText(6)
+outName = arcpy.GetParameterAsText(7)
 
-dcalcBB = 25.0
+dcalcBB = 500.0
 dcalcFT = 100.0
 
 arcpy.CheckOutExtension("Spatial")
@@ -189,90 +192,162 @@ arcpy.Near_analysis(inputFC, inputFC)
 with arcpy.da.SearchCursor(inputFC, ['FID', 'Shape@', 'Shape_Area', 'CC', 'd', 'NEAR_DIST']) as cursor:
     for row in cursor:
 
-        # Set up loop
-        # for each feature, derive distance grid - Euclidean Distance
-        # distances grids used by Nitrogen, Bellbird and Fantail ESs only
-        # Cooling uses custom distance grids
-        # get FID here for iterating
         fid = row[0]
-        distOut = "dist_" + str(fid) + ".tif"
-        arcpy.env.workspace = ws
-        # maxDist = '#' # can use ""?
-        dist = arcpy.sa.EucDistance(row[1], "", cellSize)
-        dist.save(distOut)
+        dcool = 5
 
-        # Calculate cooling raster
-        distIn = Raster(distOut)
-
-        # Calculate individual ESs
-        # Cooling and Bellbird habitat here
-        # select all SPUs greater than 4900 m2 in area and those smaller that are within 2*R of another SPU
-        # new Cooling code here
-        # For each SPU, create centerline and points around boundary
-        # For each point, calculate distance to centerline
-        # Use distance as Max Distance to calculate distance then calculate cooling effect and later mosaic to
-        # new raster.
-        arcpy.AddMessage("Starting cooling calculations")
         listCool = []
-        if row[2] >= 4900.00 or (row[2] < 4900.00 and row[5] <= float(2 * ((row[2] / math.pi) ** 0.5))):
+        listBB = []
 
-            # cc = float(row[3])
-            cc = 10
+        # check if right size/proximity for cooling
+        if row[2] >= 4900.00 or (row[2] < 4900.00 and row[5] <= float(2 * ((row[2] / math.pi) ** 0.5))):
+            print("SPU FID " + str(fid) + " is okay for cooling")
+            # cc = float(row[3]) - now set by user
 
             # new Cooling code here
             # polygon to line
-            arcpy.AddMessage("Creating SPU Line")
+            # If the right conditions, make points
+            # arcpy.AddMessage("Creating SPU Line")
+            print("Creating SPU Line")
             outLine = "SPULine_" + str(fid) + ".shp"
-            arcpy.AddMessage("Creating points on SPU Line")
+            # arcpy.AddMessage("Creating points on SPU Line")
+            print("Creaing points on SPU Line")
             arcpy.PolygonToLine_management(row[1], outLine, "")
             # autogenerate points
             bdyp = "BDYPoints_" + str(fid) + ".shp"
-            arcpy.GeneratePointsAlongLines_management(outLine, bdyp, 'DISTANCE', Distance="5 meters", Include_End_Points='END_POINTS')
+            arcpy.GeneratePointsAlongLines_management(outLine, bdyp, 'DISTANCE', Distance="5 meters",
+                                                      Include_End_Points='END_POINTS')
             # Near boundary points to centerline
             if int(arcpy.GetCount_management(bdyp)["row_count"]) > 0:
                 arcpy.Near_analysis(bdyp, cline)
                 minDistC = (max([cur[0] for cur in arcpy.da.SearchCursor(bdyp, "NEAR_DIST")]) / dcool)
-                arcpy.AddMessage("minDistC = " + str(minDistC))
+                # arcpy.AddMessage("minDistC = " + str(minDistC))
+                print("minDistC = " + str(minDistC))
 
-                # Loop through BDYPoints and do cooling calcs
-                with arcpy.da.SearchCursor(bdyp, ['FID', 'SHAPE@', 'NEAR_DIST']) as cursorp:
-                    for rowp in cursorp:
-                        pfid = rowp[0]
-                        if rowp[2] <= minDistC:
-                            d = minDistC
-                        else:
-                            d = rowp[2]
-                        distOutC = "cooldist_" + str(fid) + "_" + str(pfid) + ".tif"
-                        dist = arcpy.sa.EucDistance(rowp[1], d, cellSize)
-                        dist.save(distOutC)
+                # find max NEAR_DIST to centerline
+                maxWidth = 2 * max([cur[0] for cur in arcpy.da.SearchCursor(bdyp, "NEAR_DIST")])
+                print("maxWidth = " + str(maxWidth))
 
-                        # calculate cooling effect
-                        distInC = Raster(distOutC)
-                        ha = float(row[2])/10000
+                # if maxWidth . 5, loop through BDYPoints and do cooling calcs
+                if maxWidth >= 5:
+                    with arcpy.da.SearchCursor(bdyp, ['FID', 'SHAPE@', 'NEAR_DIST']) as cursorp:
+                        for rowp in cursorp:
+                            pfid = rowp[0]
+                            if rowp[2] <= minDistC:
+                                d = minDistC
+                            else:
+                                d = rowp[2]
+                            # if 2 * max.NEAR_DIST > 5, create disance grid
+                            distOutC = "cooldist_" + str(fid) + "_" + str(pfid) + ".tif"
+                            dist = arcpy.sa.EucDistance(rowp[1], "", cellSize)
+                            dist.save(distOutC)
 
-                        coolOut = ha * cc * Exp(-5 * (distInC / d))
-                        coolOut.save("coolcalc_" + str(fid) + "_" + str(pfid) + ".tif")
-                        listCool.append(Raster("coolcalc_" + str(fid) + "_" + str(pfid) + ".tif"))
+                            # calculate cooling effect
+                            distInC = Raster(distOutC)
+                            ha = float(row[2]) / 10000
 
-            # del rowp
-            # del cursorp
+                            print("Calculating " + str(pfid) + " cooling layer")
+                            if d >= 5:
+                                # calc cooling using CON
+                                coolOut = Con(distInC < d, ha * cc * Exp(-5 * (distInC / d)), 0)
+                                coolOut.save("coolcalc_" + str(fid) + "_" + str(pfid) + ".tif")
+                                # add outputs to list
+                                listCool.append(Raster("coolcalc_" + str(fid) + "_" + str(pfid) + ".tif"))
+                else:
+                    print("SPU FID " + str(fid) + " isn't okay for cooling")
+        # arcpy.AddMessage("Cooling calcs done, starting BB calcs")
+        print("Cooling steps done, checking BB calcs (print)")
 
-            # make cooling overlaps
-            if len(listCool) > 0:
-                overlapC = CellStatistics(listCool, "SUM", "NODATA")
-                overlapCreclass = Con(IsNull(Raster(overlapC)), 0, 1)
-                overlapCreclass.save("OverlapC_" + str(fid) + ".tif")
-
-            listCool.clear()
-
-            arcpy.AddMessage("Cooling calcs done.")
-
+        # check if right size/proximity for BB
         if row[2] >= 15000 or (row[2] > 950 and row[5] < 10.0):
-            lacaBBOut = Con(distIn <= dcalcBB,
-                            ((1 / dcalcBB) * 1.094 * (1 - (1 / dcalcBB) ** 2 * Raster(distIn) ** 2) ** 3), 0)
-            lacaBBOut.save("lacaBB_" + str(fid) + ".tif")
+            # if right conditions - check if bdy points layer exists
+            if arcpy.Exists(bdyp):
+                if maxWidth >= 25:
+                    with arcpy.da.SearchCursor(bdyp, ['FID', 'SHAPE@', 'NEAR_DIST']) as cursorbb:
+                        for rowbb in cursorbb:
+                            pfid = rowbb[0]
+                            if rowbb[2] >= 25:
+                                if arcpy.Exists("cooldist_" + str(fid) + "_" + str(pfid) + ".tif"):
+                                    print("SPU FID " + str(fid) + "_" + str(pfid) + " is all good for BB calc")
 
-        arcpy.AddMessage("Bellbird calcs done")
+                                    # if yes, calc BB using CON
+                                    bbcalc = "bbcalc_" + str(fid) + "_" + str(pfid) + ".tif"
+                                    lacaBBOut = Con(Raster("cooldist_" + str(fid) + "_" + str(pfid) + ".tif") <= dcalcBB,
+                                                    ((1 / dcalcBB) * 1.094 * (1 - (1 / dcalcBB) ** 2 * Raster("cooldist_" + str(fid) + "_" + str(pfid) + ".tif") ** 2) ** 3), 0)
+                                    lacaBBOut.save(bbcalc)
+                                    listBB.append(bbcalc)
+                                    print("Cooldist exists - BB calculated")
+                                # open search cursor to iterate through each point
+                                else:
+                                    with arcpy.da.SearchCursor(bdyp, ['FID', 'SHAPE@', 'NEAR_DIST']) as cursorp2:
+                                        print("Inside BB search cursor")
+                                        for rowp in cursorp2:
+                                            pfid = rowp[0]
+                                            if rowp[2] <= minDistC:
+                                                d = minDistC
+                                            else:
+                                                d = rowp[2]
+                                            print("d = " + str(d))
+                                            # if 2 * max.NEAR_DIST > 5, create disance grid
+                                            distOutBB = "bbdist_" + str(fid) + "_" + str(pfid) + ".tif"
+                                            dist = arcpy.sa.EucDistance(rowp[1], "", cellSize)
+                                            dist.save(distOutBB)
+
+                                            # calc BB using CON
+                                            lacaBBOut = Con(dist <= dcalcBB,
+                                                            ((1 / dcalcBB) * 1.094 * (
+                                                                    1 - (1 / dcalcBB) ** 2 * Raster("bbdist_" + str(fid) + "_" + str(pfid) + ".tif") ** 2) ** 3), 0)
+                                            lacaBBOut.save(bbcalc)
+                                            listBB.append(bbcalc)
+
+                                            print("DD calc with existing layers - cooldist didn't exist so I made one")
+                                        cursor.reset()
+
+            else:
+                # If the right conditions, make points
+                print("No cooldist exists - creating now")
+                # arcpy.AddMessage("Creating BB SPU Line")
+                outLine = "SPULine_" + str(fid) + ".shp"
+                # arcpy.AddMessage("Creating points on BB SPU Line")
+                arcpy.PolygonToLine_management(row[1], outLine, "")
+                # autogenerate points
+                bdyp = "BDYPoints_" + str(fid) + ".shp"
+                arcpy.GeneratePointsAlongLines_management(outLine, bdyp, 'DISTANCE', Distance="5 meters",
+                                                          Include_End_Points='END_POINTS')
+                # Near boundary points to centerline
+                if int(arcpy.GetCount_management(bdyp)["row_count"]) > 0:
+                    arcpy.Near_analysis(bdyp, cline)
+                    minDistBB = (max([cur[0] for cur in arcpy.da.SearchCursor(bdyp, "NEAR_DIST")]) / dcool)
+                    # arcpy.AddMessage("minDistBB = " + str(minDistBB))
+                    print("minDistBB = " + str(minDistBB))
+
+                    # find max NEAR_DIST to centerline
+                    maxWidth = 2 * max([cur[0] for cur in arcpy.da.SearchCursor(bdyp, "NEAR_DIST")])
+
+                    # if maxWidth > 25, loop through BDYPoints and do cooling calcs
+                    if maxWidth > 25:
+                        with arcpy.da.SearchCursor(bdyp, ['FID', 'SHAPE@', 'NEAR_DIST']) as cursorp:
+                            for rowp in cursorp:
+                                pfid = rowp[0]
+                                if rowp[2] <= minDistC:
+                                    d = minDistC
+                                else:
+                                    d = rowp[2]
+                                # if 2 * max.NEAR_DIST > 5, create disance grid
+                                if d >= 25:
+                                    distOutBB = "bbdist_" + str(fid) + "_" + str(pfid) + ".tif"
+                                    dist = arcpy.sa.EucDistance(rowp[1], "", cellSize)
+                                    dist.save(distOutBB)
+
+                                    # calc BB using CON
+                                    lacaBBOut = Con(dist <= dcalcBB,
+                                                    ((1 / dcalcBB) * 1.094 * (
+                                                                1 - (1 / dcalcBB) ** 2 * Raster("bbdist_" + str(fid) + "_" + str(pfid) + ".tif") ** 2) ** 3), 0)
+                                    lacaBBOut.save(distOutBB)
+                                    listBB.append(distOutBB)
+        else:
+            print("SPU FID " + str(fid) + " isn't right for BB calc")
+        # arcpy.AddMessage("Bellbird calcs done")
+        print("Bellbird steps done")
 
 # Nitrogen here
         # nOut = Con(distIn <= 7, (-3.9 * distIn ** 3 + 89.1 * distIn ** 2 - (814.5 * distIn) + 2968), 0)
