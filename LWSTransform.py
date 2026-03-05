f90f31b8847883bc9e2cdc5808e786b81777af9e
CrileNZL <crile.doscher@lincoln.ac.nz>
2026-03-05 17:53:25 +1300

    5 March 26 - adding functions for remaining metrics

--- LWSTransform.py
+++ LWSTransform.py
@@ -8,16 +8,52 @@ from arcpy import env
 
 arcpy.env.overwriteOutput = True
 
+bdy = r"G:\LandscapeMAX\Clint\BDY.gdb\Catchment_Export"
+extent = arcpy.Describe(bdy).extent
+sr = arcpy.SpatialReference(2193)
+
 # set working directory
-ws = arcpy.env.workspace = r"G:\LandscapeMAX\Clint\""
+ws = arcpy.env.workspace = r"G:\LandscapeMAX\Clint\Test"
 
-# Read in runoff raster
-runoff = arcpy.Raster(r"G:\LandscapeMAX\Clint\TIFFs\runoff.tif")
+# generate tesselations
+tesselation = arcpy.management.GenerateTessellation(r"G:\LandscapeMAX\Clint\LMax\tiles.shp", extent, "TRANSVERSE_HEXAGON", "2 Hectares", sr)
+# whereclause1 = "'tesselation', 'INTERSECT', 'bdy'"
+print("Hexagons done"
+      "")
+select = arcpy.management.SelectLayerByLocation(tesselation,'INTERSECT', bdy)
+arcpy.conversion.ExportFeatures(select, r"G:\LandscapeMAX\Clint\LMAX\Hex.shp")
+print("New Hexes done")
 
-# Calculate L+ raster
-ROPos = runoff * 5
-ROPos.save(r"G:\LandscapeMAX\Clint\LMax\ROPos.tif")
+# Take LWS inputs and transform to Pos, Inv or Log outputs
+# ee = extreme event, dr = drainage, fa = flow accumulation, ro = runoff propensity, wp = wetness persistence
+nameList = ["ee", "dr", "fa", "ro", "wp"]
+rasList = arcpy.ListRasters()
 
-# Calculate L- raster
-ROInv = (1 - runoff) * 5
-ROInv.save(r"G:\LandscapeMAX\Clint\LMax\ROInv.tif")
+for raster in rasList:
+      ras = Raster(raster)
+      for name in nameList:
+            pos = ras * 5
+            pos.save(name + r"Pos.tif")
+            inv = (1 - ras) * 5
+            inv.save(name + "Inv.tif")
+            log = ROLog = 5 / (1 + (49 * Exp(-10 * ras)))
+            log.save(name + "Log.tif")
+
+# calculate LMAX cases for trees and wetlands
+# get LMAX layers
+lmaxList = arcpy.ListRasters("*_*")
+
+treeh = (3*Raster("ro_Pos.tif")) + (3*Raster("wp_Pos.tif")) + (2*Raster("ee_Pos.tif")) + (3*Raster("fa_Pos.tif")) + (3*Raster("dr_Pos.tif"))
+treeh.save("treeh.tif")
+
+treenh = ((2*Raster("ro_Pos.tif")) + Raster("ro_Inv.tif")) + ((2*Raster("wp_Inv.tif")) + Raster("wp_Pos.tif")) + (2*Raster("ee_Pos.tif")) + ((2*Raster("fa_Pos.tif")) + Raster("fa_Inv.tif")) + ((2*Raster("dr_Pos.tif")) + Raster("dr_Inv.tif"))
+treenh.save("treenh.tif")
+
+wetland = ((2*Raster("ro_Pos.tif")) + Raster("ro_Inv.tif")) + ((2*Raster("wp_Pos.tif")) + Raster("wp_Log.tif")) + (Raster("ee_Log.tif") + Raster("ee_Inv.tif")) + ((2*Raster("fa_Pos.tif")) + Raster("fa_Inv.tif")) + (3*Raster("dr_Pos.tif"))
+wetland.save(wetland.tif)
+
+# Zonal Statistics as Table
+# arcpy.sa.ZonalStatisticsAsTable(r"G:\LandscapeMAX\Clint\LMAX\Hex.shp", "GRID_ID", r"G:\LandscapeMAX\Clint\LMAX\ROPos.tif", r"G:\LandscapeMAX\Clint\LMAX\ROPosOut.dbf", "DATA", "MEAN", "", "", "", "", "", "ROPosJoin")
+# print("ZSaT done")
+
+# arcpy.management.CopyFeatures("ROPosJoin", r"G:\LandscapeMAX\Clint\LMAX\ROPos.shp")
